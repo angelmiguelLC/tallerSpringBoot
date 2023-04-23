c0bee2edbfb9a5a99405282ce907392ac4658872
MiguelLedesmaC <angelmiguellc35@gmail.com>
2023-04-23 17:46:35 +0200

    añado metodo handleConflict clase RestResponseEntity

--- src/main/java/es/bootcamp/pruebaMsSpringboot/controller/RestResponseEntityExceptionHandler.java
+++ src/main/java/es/bootcamp/pruebaMsSpringboot/controller/RestResponseEntityExceptionHandler.java
@@ -1,5 +1,19 @@
 package es.bootcamp.pruebaMsSpringboot.controller;
 
-public class RestResponseEntityExceptionHandler {
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import es.bootcamp.pruebaMsSpringboot.model.User;
+
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	@ExceptionHandler(value= {IllegalArgumentException.class, IllegalStateException.class})
+	protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request){
+		String bodyOfResponse = "This should be application specific";
+		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(),HttpStatus.CONFLICT, request);
+	}
 }
